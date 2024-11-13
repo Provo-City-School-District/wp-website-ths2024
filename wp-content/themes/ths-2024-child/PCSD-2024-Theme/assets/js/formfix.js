b53836b870908390072f179d6a58c1f33e7028bf
Josh Espinoza <joshespi@gmail.com>
2024-11-13 13:50:40 -0700

    remove duplicate theme files and assets

--- wp-content/themes/ths-2024-child/PCSD-2024-Theme/assets/js/formfix.js
+++
@@ -1,6 +0,0 @@
-//forms with textarea with ID "message". when focused it should scroll to place the textarea at the top of the viewport to the best of its ability.
-let formMessageInput = document.getElementById("textareamessage");
-
-formMessageInput.addEventListener("focus", function () {
-  formMessageInput.scrollIntoView({ block: "center" });
-});
