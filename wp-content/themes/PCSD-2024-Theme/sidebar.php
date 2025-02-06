b028629e8a6eb6b518323dffcc1f96fc1be4df02
Josh Espinoza <joshespi@gmail.com>
2025-02-06 06:55:57 -0700

    new sidebar update

--- wp-content/themes/PCSD-2024-Theme/sidebar.php
+++ wp-content/themes/PCSD-2024-Theme/sidebar.php
@@ -1,6 +1,6 @@
 <aside id="rightSidebar" class="rightSidebar">
 	<?php
-	$theme_vars = my_theme_variables();
+	// $theme_vars = my_theme_variables();
 	if (is_page('teachers-staff')) {
 	?>
 		<label for="dsearch" class="hidden" id="directorySearch">Directory Search: </label>
@@ -8,44 +8,14 @@
 		<img class="directorySearchIcon" src="//globalassets.provo.edu/image/icons/search-lt.svg" alt="" />
 	<?php
 	}
-
 	?>
-	<h2>Follow Us</h2>
-	<ul class="sociallinks">
-		<?php
-		if (isset($theme_vars['insta_link'])) {
-		?>
-		<li><a href="<?php echo $theme_vars['insta_link'] ?>"><?php echo get_svg('socialmedia-insta'); ?></a></li>
-		<?php
-		}
-		if (isset($theme_vars['twitter_link'])) {
-		?>
-			<li><a href="<?php echo $theme_vars['twitter_link'] ?>"><?php echo get_svg('socialmedia-twitter'); ?></a></li>
-		<?php
-		}
-		if (isset($theme_vars['facebook_link'])) {
-		?>
-		<li><a href="<?php echo $theme_vars['facebook_link'] ?>"><?php echo get_svg('socialmedia-facebook'); ?></a></li>
-		<?php
-		}
-		?>
-	</ul>
 	<?php
-	//load sidebar calendars
-	if (isset($theme_vars['top_sidebar_cal'])) {
-		echo '<h2>A/B Calendar</h2>';
-		echo do_shortcode($theme_vars['top_sidebar_cal']);
-	}
-	//load sidebar menu if it exists
-	if (function_exists('sidebar_menu')) {
-		sidebar_menu();
-	}
-	if (isset($theme_vars['bot_sidebar_cal'])) {
-		echo '<section class="impDates">';
-		echo '<h2>Important Dates</h2>';
-		echo do_shortcode($theme_vars['bot_sidebar_cal']);
-		echo '</section>';
+	$sidebar_page_id = get_field('sidebar');
+	if ($sidebar_page_id) {
+		$sidebar_content = get_post($sidebar_page_id);
+		echo apply_filters('the_content', $sidebar_content->post_content);
+	} else {
+		echo 'sidebar not set';
 	}
 	?>
-
 </aside>
--- wp-content/themes/ths-2024-child/functions.php
+++ wp-content/themes/ths-2024-child/functions.php
@@ -8,11 +8,11 @@ function my_theme_variables()
         'short_school_name' => 'Timpview High',
         'school_address' => '3570 Timpview Dr Provo, Utah 84604',
         'google_tag_manager_id' => 'G-FTPJPV04N2',
-        'top_sidebar_cal' => '[calendar id="2064"]',
-        'bot_sidebar_cal' => '[calendar id="2066"]',
+        // 'top_sidebar_cal' => '[calendar id="2064"]',
+        // 'bot_sidebar_cal' => '[calendar id="2066"]',
         'front_page_cal' => '[calendar id="2069"]',
-        'insta_link' => 'http://#',
-        'facebook_link' => 'http://#',
+        // 'insta_link' => 'http://#',
+        // 'facebook_link' => 'http://#',
         // 'twitter_link' => 'http://#',
         'full_calendar_link' => 'http://#',
         // 'search_icon' => get_template_directory_uri() . '/assets/icons/search-loupe.svg',
@@ -20,13 +20,13 @@ function my_theme_variables()
     );
     return $my_theme_variables;
 }
-function sidebar_menu()
-{
-    //TODO: add a wp menu here instead of hardcoding
-    echo "<ul>";
-    echo "<li><a href='https://provohigh.provo.edu/school-calendar/a-b-calendar-month-view/'>A/B Calendar Month View</a></li>";
-    echo "</ul>";
-}
+// function sidebar_menu()
+// {
+//     //TODO: add a wp menu here instead of hardcoding
+//     echo "<ul>";
+//     echo "<li><a href='https://provohigh.provo.edu/school-calendar/a-b-calendar-month-view/'>A/B Calendar Month View</a></li>";
+//     echo "</ul>";
+// }
 function pcsd_child_theme_enqueue_styles()
 {
     wp_enqueue_style('variables', get_stylesheet_directory_uri() . '/assets/css/variables.css', '', '1.0.0', false);
